e25dba6a256515464bf07b866bf4eb02ed94f08c
varunkarthi1324 <vkvarunkarthikeyan@gmail.com>
2024-09-25 23:21:57 +0530

    commited destructure function

---
+++ destructure.js
@@ -0,0 +1,21 @@
+var myob={
+    name:'varun',
+    age:19,
+    job:'fullstack',
+    address:{
+        area:'tindivanam',
+        city:'chennai',
+        pin:604001
+    }
+}
+
+var {name:n,age:a,job:j,address:{area:ar,city:c,pin:p}}=myob
+
+console.log(n);
+console.log(a);
+console.log(j);
+console.log(ar);
+console.log(c);
+console.log(p);
+
+
