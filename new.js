112c5bd5a5038df4200990dfa729ebf371f25f02
varunkarthi1324 <vkvarunkarthikeyan@gmail.com>
2024-09-25 23:20:54 +0530

    ne wfiles are added

---
+++ new.js
@@ -0,0 +1,9 @@
+/*const calc=require('./module')
+
+calc.addition(2,3)
+calc.subtraction(13,24)
+calc.multiplication(13,24)
+calc.division(13,24)*/
+
+const calsqare=(a)=> (a*a % 2 === 0 ? true:false)
+console.log(calsqare(123456486987));
