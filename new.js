ed17753bf2cc884f61bf85b4532f77bd97567c64
varunkarthi1324 <vkvarunkarthikeyan@gmail.com>
2024-09-28 19:59:12 +0530

    commited newjs

--- new.js
+++ new.js
@@ -5,5 +5,6 @@ calc.subtraction(13,24)
 calc.multiplication(13,24)
 calc.division(13,24)*/
 
+//it is an example for if else short function using arrow fn 
 const calsqare=(a)=> (a*a % 2 === 0 ? true:false)
 console.log(calsqare(123456486987));
